51464d82bfa08eeff820a9a240725dce22febdce
unknown <Cetecom@SJAPCA03LC0L7.duoc.acad>
2025-06-11 14:15:04 -0400

    mi primer commit

---
+++ app.py
@@ -0,0 +1,18 @@
+
+def sumar(a, b):
+    print(a+b)
+
+sumar(2, 4)
+
+def saludo(nombre):
+    print(f"Hola, {nombre}")
+
+saludo("Patricio")
+
+def es_par(num):
+    if num % 2 == 0:
+        print(f"El numero {num} es par")
+    else:
+        print(f"El numero {num} es inpar")
+
+es_par(10)
